6030812c0e9e14e487143f0543b5729fda5ae50c
Jason Bosco <mail@jasonbos.co>
2021-01-05 19:13:44 -0800

    Update schema

--- scraper/src/typesense_helper.py
+++ scraper/src/typesense_helper.py
@@ -24,14 +24,19 @@ class TypesenseHelper:
 
     def create_tmp_collection(self):
         """Create a temporary index to add records to"""
-        self.typesense_client.collections[self.collection_name_tmp].delete()
+        try:
+            self.typesense_client.collections[self.collection_name_tmp].delete()
+        except exceptions.ObjectNotFound:
+            pass
+
         self.typesense_client.collections.create({
             'name': self.collection_name_tmp,
             'fields': [
                 {'name': 'anchor', 'type': 'string', 'optional': True},
                 {'name': 'content', 'type': 'string', 'optional': True},
-                {'name': 'url', 'type': 'string'},
-                {'name': 'item_priority', 'type': 'int32'},
+                {'name': 'url', 'type': 'string', 'facet': True},
+                {'name': 'version', 'type': 'string', 'facet': True},
+                {'name': 'item_priority', 'type': 'int64'},
             ],
             'default_sorting_field': 'item_priority'
         })
@@ -45,7 +50,8 @@ class TypesenseHelper:
             result = self.typesense_client.collections[self.collection_name_tmp].documents.import_(
                 transformed_records[i:i + 50])
             failed_items = list(
-                map(lambda r: json.loads(json.loads(r)), filter(lambda r: json.loads(json.loads(r))['success'] is False, result)))
+                map(lambda r: json.loads(json.loads(r)),
+                    filter(lambda r: json.loads(json.loads(r))['success'] is False, result)))
             if len(failed_items) > 0:
                 print(failed_items)
                 raise Exception
@@ -68,10 +74,12 @@ class TypesenseHelper:
         self.typesense_client.aliases.upsert(self.alias_name, {'collection_name': self.collection_name_tmp})
 
         if old_collection_name:
-            self.typesense_client.collections(old_collection_name).delete()
+            self.typesense_client.collections[old_collection_name].delete()
 
     @staticmethod
     def transform_record(record):
         transformed_record = {k: v for k, v in record.items() if v is not None}
-        transformed_record['item_priority'] = transformed_record.get('item_priority', 10)
+        transformed_record['item_priority'] = transformed_record['weight']['page_rank'] * 1000000000 + \
+                                              transformed_record['weight']['level'] * 1000 + \
+                                              transformed_record['weight']['position']
         return transformed_record
