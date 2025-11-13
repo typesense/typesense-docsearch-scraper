c563d2bcca5fe0f1e7361a89137f806d95dbc40f
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-11-13 17:56:23 +0200

    feat: support v30+ versions of typesense

--- scraper/src/typesense_helper.py
+++ scraper/src/typesense_helper.py
@@ -35,6 +35,11 @@ class TypesenseHelper:
         self.collection_name_tmp = collection_name_tmp
         self.collection_locale = os.environ.get('TYPESENSE_COLLECTION_LOCALE', 'en')
         self.custom_settings = custom_settings
+        version = self.typesense_client.debug.retrieve().get("version")
+        if version == "nightly":
+            self.typesense_version = 30
+        else:
+            self.typesense_version = int(version.split(".")[0])
 
     def create_tmp_collection(self):
         """Create a temporary index to add records to"""
@@ -43,105 +48,125 @@ class TypesenseHelper:
         except exceptions.ObjectNotFound:
             pass
 
-        schema = {
-            'name': self.collection_name_tmp,
-            'fields': [
-                {'name': 'anchor', 'type': 'string', 'optional': True},
+        general_schema = {
+            "name": self.collection_name_tmp,
+            "fields": [
+                {"name": "anchor", "type": "string", "optional": True},
                 {
-                    'name': 'content',
-                    'type': 'string',
-                    'locale': self.collection_locale,
-                    'optional': True,
+                    "name": "content",
+                    "type": "string",
+                    "locale": self.collection_locale,
+                    "optional": True,
                 },
-                {'name': 'url', 'type': 'string', 'facet': True},
+                {"name": "url", "type": "string", "facet": True},
                 {
-                    'name': 'url_without_anchor',
-                    'type': 'string',
-                    'facet': True,
-                    'optional': True,
-                },
-                {
-                    'name': 'version',
-                    'type': 'string[]',
-                    'facet': True,
-                    'optional': True,
+                    "name": "url_without_anchor",
+                    "type": "string",
+                    "facet": True,
+                    "optional": True,
                 },
                 {
-                    'name': 'hierarchy.lvl0',
-                    'type': 'string',
-                    'facet': True,
-                    'locale': self.collection_locale,
-                    'optional': True,
+                    "name": "version",
+                    "type": "string[]",
+                    "facet": True,
+                    "optional": True,
                 },
                 {
-                    'name': 'hierarchy.lvl1',
-                    'type': 'string',
-                    'facet': True,
-                    'locale': self.collection_locale,
-                    'optional': True,
+                    "name": "hierarchy.lvl0",
+                    "type": "string",
+                    "facet": True,
+                    "locale": self.collection_locale,
+                    "optional": True,
                 },
                 {
-                    'name': 'hierarchy.lvl2',
-                    'type': 'string',
-                    'facet': True,
-                    'locale': self.collection_locale,
-                    'optional': True,
+                    "name": "hierarchy.lvl1",
+                    "type": "string",
+                    "facet": True,
+                    "locale": self.collection_locale,
+                    "optional": True,
                 },
                 {
-                    'name': 'hierarchy.lvl3',
-                    'type': 'string',
-                    'facet': True,
-                    'locale': self.collection_locale,
-                    'optional': True,
+                    "name": "hierarchy.lvl2",
+                    "type": "string",
+                    "facet": True,
+                    "locale": self.collection_locale,
+                    "optional": True,
                 },
                 {
-                    'name': 'hierarchy.lvl4',
-                    'type': 'string',
-                    'facet': True,
-                    'locale': self.collection_locale,
-                    'optional': True,
+                    "name": "hierarchy.lvl3",
+                    "type": "string",
+                    "facet": True,
+                    "locale": self.collection_locale,
+                    "optional": True,
                 },
                 {
-                    'name': 'hierarchy.lvl5',
-                    'type': 'string',
-                    'facet': True,
-                    'locale': self.collection_locale,
-                    'optional': True,
+                    "name": "hierarchy.lvl4",
+                    "type": "string",
+                    "facet": True,
+                    "locale": self.collection_locale,
+                    "optional": True,
                 },
                 {
-                    'name': 'hierarchy.lvl6',
-                    'type': 'string',
-                    'facet': True,
-                    'locale': self.collection_locale,
-                    'optional': True,
+                    "name": "hierarchy.lvl5",
+                    "type": "string",
+                    "facet": True,
+                    "locale": self.collection_locale,
+                    "optional": True,
                 },
                 {
-                    'name': 'type',
-                    'type': 'string',
-                    'facet': True,
-                    'locale': self.collection_locale,
-                    'optional': True,
+                    "name": "hierarchy.lvl6",
+                    "type": "string",
+                    "facet": True,
+                    "locale": self.collection_locale,
+                    "optional": True,
                 },
                 {
-                    'name': '.*_tag',
-                    'type': 'string',
-                    'facet': True,
-                    'locale': self.collection_locale,
-                    'optional': True,
+                    "name": "type",
+                    "type": "string",
+                    "facet": True,
+                    "locale": self.collection_locale,
+                    "optional": True,
                 },
-                {'name': 'language', 'type': 'string', 'facet': True, 'optional': True},
                 {
-                    'name': 'tags',
-                    'type': 'string[]',
-                    'facet': True,
-                    'locale': self.collection_locale,
-                    'optional': True,
+                    "name": ".*_tag",
+                    "type": "string",
+                    "facet": True,
+                    "locale": self.collection_locale,
+                    "optional": True,
                 },
-                {'name': 'item_priority', 'type': 'int64'},
+                {"name": "language", "type": "string", "facet": True, "optional": True},
+                {
+                    "name": "tags",
+                    "type": "string[]",
+                    "facet": True,
+                    "locale": self.collection_locale,
+                    "optional": True,
+                },
+                {"name": "item_priority", "type": "int64"},
             ],
-            'default_sorting_field': 'item_priority',
-            'token_separators': ['_', '-'],
+            "default_sorting_field": "item_priority",
+            "token_separators": ["_", "-"],
         }
+
+        if self.typesense_version >= 30:
+            try:
+                old_collection = self.typesense_client.collections[
+                    self.collection_name_tmp
+                ].retrieve()
+                curation_sets = old_collection.get("curation_sets", [])
+
+                synonym_sets = old_collection.get("synonym_sets", [])
+            except exceptions.ObjectNotFound:
+                curation_sets = []
+                synonym_sets = []
+
+            schema = {
+                **general_schema,
+                "curation_sets": curation_sets,
+                "synonym_sets": synonym_sets,
+            }
+        else:
+            schema = general_schema
 
         if self.custom_settings is not None:
             token_separators = self.custom_settings.get('token_separators', None)
@@ -238,6 +263,9 @@ class TypesenseHelper:
 
     def _transfer_synonyms(self, old_collection_name):
         """Transfer synonyms from old collection to new collection"""
+        if self.typesense_version >= 30:
+            return
+
         synonyms = (
             self.typesense_client.collections[old_collection_name]
             .synonyms.retrieve()
@@ -251,6 +279,9 @@ class TypesenseHelper:
 
     def _transfer_overrides(self, old_collection_name):
         """Transfer overrides from old collection to new collection"""
+        if self.typesense_version >= 30:
+            return
+
         overrides = (
             self.typesense_client.collections[old_collection_name]
             .overrides.retrieve()
